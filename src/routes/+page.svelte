171db27d0909b03ec2479e2ae6b04d477d283435
Kevin <125473461+viizion-03@users.noreply.github.com>
2024-06-14 15:15:39 +0200

    tailwind css installation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+   const greet = "something" 
+</script>
+
+<h1 class="text-2xl text-blue-400">somthing nice here</h1>
+{greet}
