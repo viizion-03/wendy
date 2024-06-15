340275dd24d905384b2cc22e63a2184834f8d71b
Kevin <125473461+viizion-03@users.noreply.github.com>
2024-06-15 21:44:22 +0200

    final touch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -86,23 +86,18 @@
 	let player;
 
 	let showPlayer = false;
-
-    let playbutton = document.getElementsByClassName("playButton")
-    console.log(playbutton)
-
-    
 </script>
 
 <div class="absolute h-full w-full bg-sunset opacity-15 bg-cover overflow-hidden"></div>
 <div></div>
 <iframe
-	class="absolute top-0 z-10"
+	class="{showPlayer ? 'block' : 'hidden'} absolute top-0 z-10"
 	width="100%"
 	height="100"
 	scrolling="no"
 	frameborder="no"
 	allow="autoplay"
-	src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/770717896&color=%231be7ec&auto_play=true&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+	src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/770717896&color=%231be7ec&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
 ></iframe>
 <!-- <div
 	style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"
